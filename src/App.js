93ff51a53a9b4b94cec3a35cfb1867bd2c12e505
Rovan <rovan@heubert.com>
2022-11-07 14:46:32 +0545

    create event in google calendar through fullcalendar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css'
-import events from './events'
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import moment from 'moment'
 import CustomModal from './components/CustomModal'
 import DateRangePicker from 'react-bootstrap-daterangepicker'
+// import { DatePicker, Space } from 'antd'
 import { FormGroup, Label, Input } from 'reactstrap'
 import { gapi } from 'gapi-script'
 
-let todayStr = new Date().toISOString().replace(/T.*$/, '')
-
-const apiKEY = 'AIzaSyA0SwXJDL2P09zUazraFeq0Rk1W3r_pvf4'
+// const apiKEY = 'AIzaSyA0SwXJDL2P09zUazraFeq0Rk1W3r_pvf4'
 const clientId = '843203138930-kor2v2jk15o9sqt76qmagckdr7slkqsq.apps.googleusercontent.com'
 // const access_token = 'ya29.a0Aa4xrXOFsKdO5vWQgmcIpSJSroU3cuQ8DIGsqRXchUcxf5dl3K9blDgPrmdGZ9rU9EcbLxQY9LAp9i5ZzSlefAnktucVkGX405ofLUh9tVl5GqF80RkaTGFqLNZr6LLCQrksv5Ne9D-TIAD_2Ycn5POtL0V-aCgYKATASARESFQEjDvL9uM3YTbkj0-T5WvLVX0TvBw0163'
 
@@ -29,42 +26,57 @@ function App() {
 	const [title, setTitle] = useState('')
 	const [start, setStart] = useState(new Date())
 	const [end, setEnd] = useState(new Date())
-	const [events, setEvents] = useState([
-		{
-			id: '1b',
-			title: 'All-day event',
-			start: todayStr,
-			// date: "2020-07-29"
-		},
-		{
-			id: '1a',
-			title: 'Timed event',
-			start: todayStr + 'T12:00:00',
-			end: todayStr + 'T12:30:00',
-			// date: "2020-07-30"
-		},
-	])
+	const [events, setEvents] = useState([])
 	const [modal, setModal] = useState(false)
 	const [state, setState] = useState({})
 	const [confirmModal, setConfirmModal] = useState(false)
 
 	const calendarRef = useRef(null)
 
-	function handleSubmit() {
-		const newEvent = {
-			id: 'asfdsa',
-			title,
-			start: state.selectInfo?.startStr || start.toISOString(),
-			end: state.selectInfo?.endStr || end.toISOString(),
-			allDay: state.selectInfo?.allDay || false,
+	const addEvent = (event) => {
+		// function initiate() {
+		// 	var request = gapi.client.calendar.events.insert({
+		// 		calendarId: calendarID,
+		// 		resource: event,
+		// 	})
+		// 	request.execute((e) => {
+		// 		window.open(e.htmlLink)
+		// 	})
+		// }
+		// gapi.load('client', initiate)
+		return gapi.client.calendar.events.insert({
+			calendarId: 'primary',
+			resource: event,
+		})
+	}
+
+	async function handleSubmit() {
+		try {
+			const newEvent = {
+				summary: title,
+				start: { dateTime: state.selectInfo?.startStr || start.toISOString() },
+				end: { dateTime: state.selectInfo?.endStr || end.toISOString() },
+			}
+			//returns the created event in google calendar
+			const res = await addEvent(newEvent)
+			console.log(res)
+			logEvents()
+			handleClose()
+			// console.log(state, 'state')
+			// console.log(newEvent, 'event')
+			// let calendarApi = calendarRef.current.getApi()
+			// calendarApi.addEvent(newEvent)
+		} catch (error) {
+			console.log(error)
+			handleClose()
 		}
-		console.log(state, 'state')
-		console.log(newEvent, 'event')
+	}
 
-		let calendarApi = calendarRef.current.getApi()
-
-		calendarApi.addEvent(newEvent)
-		handleClose()
+	const updateEvent = (event) => {
+		return gapi.client.calendar.events.insert({
+			calendarId: 'primary',
+			resource: event,
+		})
 	}
 
 	function handleEdit() {
@@ -111,7 +123,7 @@ function App() {
 
 	const handleDateSelect = (selectInfo) => {
 		console.log(selectInfo)
-		console.log(moment(selectInfo.startStr).format('YYYY-MM-DD hh:mm:ss a'))
+		// console.log(moment(selectInfo.startStr).format('YYYY-MM-DD hh:mm:ss a'))
 		selectInfo.view.calendar.unselect()
 		setState({ selectInfo, state: 'create' })
 		setStart(selectInfo.start)
@@ -155,6 +167,7 @@ function App() {
 			.signIn({ scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events' })
 		await gapi.client.load('https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest')
 		console.log('signed in and client loaded')
+		logEvents()
 	}
 
 	// const handleClientLoad = () => {
@@ -186,7 +199,8 @@ function App() {
 				}
 				newEventsArr.push(event)
 			})
-			setEvents((prevArr) => [...prevArr, ...newEventsArr])
+			// setEvents((prevArr) => [...prevArr, ...newEventsArr])
+			setEvents(newEventsArr)
 		} catch (err) {
 			console.log(err)
 		}
@@ -230,52 +244,39 @@ function App() {
 	// }
 	// }, [])
 
-	const addEvent = (calendarID, event) => {
-		function initiate() {
-			var request = gapi.client.calendar.events.insert({
-				calendarId: calendarID,
-				resource: event,
-			})
-			request.execute((e) => {
-				window.open(e.htmlLink)
-			})
-		}
-		gapi.load('client', initiate)
-	}
-
 	// // Make sure the client is loaded and sign-in is complete before calling this method.
-	const execute = async () => {
-		var event = {
-			summary: 'Google I/O 2015',
-			location: '800 Howard St., San Francisco, CA 94103',
-			description: "A chance to hear more about Google's developer products.",
-			start: {
-				dateTime: '2022-10-28T09:00:00-07:00',
-				timeZone: 'America/Los_Angeles',
-			},
-			end: {
-				dateTime: '2022-10-28T17:00:00-07:00',
-				timeZone: 'America/Los_Angeles',
-			},
-			recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
-			attendees: [{ email: 'lpage@example.com' }, { email: 'sbrin@example.com' }],
-			reminders: {
-				useDefault: false,
-				overrides: [
-					{ method: 'email', minutes: 24 * 60 },
-					{ method: 'popup', minutes: 10 },
-				],
-			},
-		}
+	// const execute = async () => {
+	// 	var event = {
+	// 		summary: 'Google I/O 2015',
+	// 		location: '800 Howard St., San Francisco, CA 94103',
+	// 		description: "A chance to hear more about Google's developer products.",
+	// 		start: {
+	// 			dateTime: '2022-10-28T09:00:00-07:00',
+	// 			timeZone: 'America/Los_Angeles',
+	// 		},
+	// 		end: {
+	// 			dateTime: '2022-10-28T17:00:00-07:00',
+	// 			timeZone: 'America/Los_Angeles',
+	// 		},
+	// 		recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+	// 		attendees: [{ email: 'lpage@example.com' }, { email: 'sbrin@example.com' }],
+	// 		reminders: {
+	// 			useDefault: false,
+	// 			overrides: [
+	// 				{ method: 'email', minutes: 24 * 60 },
+	// 				{ method: 'popup', minutes: 10 },
+	// 			],
+	// 		},
+	// 	}
 
-		addEvent('primary', event)
-	}
+	// 	addEvent('primary', event)
+	// }
 
 	return (
 		<div className="App">
 			<button onClick={() => authenticate()}>authenticate</button>
-			<button onClick={() => logEvents()}>log events</button>
-			<button onClick={() => execute()}>execute</button>
+			{/* <button onClick={() => logEvents()}>log events</button> */}
+			{/* <button onClick={() => execute()}>execute</button> */}
 			<FullCalendar
 				ref={calendarRef}
 				plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -341,6 +342,9 @@ function App() {
 				</FormGroup>
 				<FormGroup>
 					<Label for="exampleEmail">From - End</Label>
+					{/* <Space>
+						<DatePicker.RangePicker showTime={{ format: 'HH:mm' }} />
+					</Space> */}
 					<DateRangePicker
 						initialSettings={{
 							locale: {
